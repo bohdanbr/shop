ae0ebc18c03fb380a75b7a0dc4672dc866bf0e77
Bohdan Braha <bohdanbrha@gmail.com>
2023-12-14 16:07:54 +0200

    add new files

---
+++ src/components/Products.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper">
+    <div class="tools">
+      <input type="radio" name="" id="">
+      <label for="">Сетка</label>
+      <input type="radio" name="" id="">
+      <label for="">Списком</label>
+    </div>
+    <div v-for="index in 10" :key="index" class="content">
+      <div class="item">
+        <div class="title"></div>
+        <div class="img"></div>
+        <div class="price"></div>
+        <div class="description"></div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
